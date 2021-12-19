077071fa8b40e0d5248747a8a04e27e36ebff9d4
Igor Akinin <akinin32167@gmail.com>
2021-12-19 14:48:41 +0700

    change by bit operations

--- IPCount/src/main/java/ip/counter/IPCounter.java
+++ IPCount/src/main/java/ip/counter/IPCounter.java
@@ -4,51 +4,60 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class IPCounter {
 
+    int[] depository = new int[1 << 27];
+    long count = 0;
+
     public long countUnique(String fileName) {
-        long count = 0;
         long start = System.currentTimeMillis();
-        Map<String, IPAddress> setIPAddress = createSet();
-        if (fileName == null) {
-            throw new IllegalArgumentException("File name can't be null!");
-        }
+        long countLines = 0;
+        int minute = 1;
         String line = null;
         try (BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)))) {
+            System.out.println("Started file read!");
             while ((line = reader.readLine()) != null) {
-                String[] inputIP = line.split("\\.");
-                IPAddress address = setIPAddress.get(inputIP[0]);
-                int firstNumber = Integer.parseInt(inputIP[1]);
-                int secondNumber = Integer.parseInt(inputIP[2]);
-                int thirdNumber = Integer.parseInt(inputIP[3]);
-                if (address.getFirstNumber(firstNumber) != 1 || address.getSecondNumber(secondNumber) != 1
-                        || address.getThirdNumber(thirdNumber) != 1) {
-                    count++;
-                    address.setFirstNumber(firstNumber);
-                    address.setSecondNumber(secondNumber);
-                    address.setThirdNumber(thirdNumber);
+                countLines++;
+                registerLongValue(toLongValue(line));
+
+                long time = System.currentTimeMillis();
+                if ((((time - start) / 1000) / minute) == 60) {
+                    minute++;
+                    System.out.println("Reading is " + (minute - 1) + " minutes, readed - " + countLines
+                            + " addresses, it's containts " + count + " unique addresses");
                 }
             }
+
         } catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
             throw new IllegalArgumentException(fileName + " containts invalid data format: " + line);
         } catch (IOException e) {
             throw new NoFileException("File \"" + fileName + "\" not found!");
         }
+
         long finish = System.currentTimeMillis();
         long analisisTime = (finish - start) / 1000;
+        System.out.println("Size this file is " + countLines + " lines");
         System.out.println("Counting unique IPv4 addresses in " + fileName + " lasted " + analisisTime / 60
                 + " minutes " + analisisTime % 60 + " seconds!");
         return count;
-    }   
+    }
 
-    private Map<String, IPAddress> createSet() {
-        Map<String, IPAddress> setIPAddress = new LinkedHashMap<>();
-        for (int i = 0; i < 256; i++) {
-            setIPAddress.put(Integer.toString(i), new IPAddress());
+    private void registerLongValue(long longValue) {
+        int index = (int) (longValue >> 5);
+        int bit = 1 << (longValue & 31);
+        if ((depository[index] & bit) == 0) {
+            count++;
+            depository[index] |= bit;
         }
-        return setIPAddress;
+    }
+
+    private long toLongValue(String ipString) throws UnknownHostException {
+        long result = 0;
+        for (byte b : InetAddress.getByName(ipString).getAddress())
+            result = (result << 8) | (b & 255);
+        return result;
     }
 }
--- IPCount/src/main/java/ip/counter/Launcher.java
+++ IPCount/src/main/java/ip/counter/Launcher.java
@@ -4,6 +4,11 @@ public class Launcher {
 
     public static void main(String[] args) {
         IPCounter counter = new IPCounter();
-        System.out.println("Unique IP adresses in this file is " + counter.countUnique("D:\\Downloads\\ip_addresses"));        
+//        System.out.println("Unique IP adresses in this file is " + counter.countUnique("D:\\Downloads\\ip_addresses"));
+        System.out.println(java.lang.Runtime.getRuntime().maxMemory());
+//        System.out.println("Unique IP adresses in this file is " + counter.countUniqueBySet("D:\\Downloads\\ip_addresses"));
+//        System.out.println("Unique IP adresses in this file is " + counter.countUniqueByMap("D:\\Downloads\\ip_addresses"));
+        System.out.println("Unique IP adresses in this file is " + counter.countUnique("D:\\Downloads\\ip_addresses"));
+//        System.out.println("Unique IP adresses in this file is " + counter.countUnique("C:\\Repositories\\IPUniqueCount\\IPCount\\src\\main\\resources\\testipfile.txt"));        
     }
 }
